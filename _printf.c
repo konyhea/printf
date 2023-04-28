58b0874f5ea109112f7372a0e84ff51c8c933ae0
konyhea <ndukaprincek@gmail.com>
2023-04-27 22:00:25 -0700

    implementer

---
+++ _printf.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _printf - a function that works like the normal printf
+ * @format: a string containing all characters and parameters
+ *
+ * Return: Always success 0
+ */
+
+int _printf(const char *format, ...)
+{
+	int count;
+
+	spec fun_list[] = {
+		{"c", print_chara},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_int},
+		{"i", print_int}, 
+	/*	{"b", print_binary}, 
+		{"u", print_unsigned}, */
+		{NULL, NULL}
+	};
+
+	va_list args;
+
+	if (format == NULL)
+	return (-1);
+
+	va_start(args, format);
+
+	count = checker(format, fun_list, args);
+	va_end(args);
+	return (count);
+}
