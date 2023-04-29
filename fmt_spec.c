c866eae672cea4d1ad9f98d7ec27d1acded6118a
konyhea <ndukaprincek@gmail.com>
2023-04-28 21:10:01 -0700

    task 0

--- fmt_spec.c
+++ fmt_spec.c
@@ -9,9 +9,16 @@
 
 int print_chara(va_list list, char *buffer, int *buffer_index)
 {
-	char c = va_arg(list, int);
+	char c;
+
+	c = va_arg(list, int);
 	buffer[*buffer_index] = c;
-	(*buffer_index)++;
+	
+	if (*buffer_index == BUFFER_SIZE)
+	{
+		write(1, buffer, BUFFER_SIZE);
+		*buffer_index = 0;
+	}	
 	return (1);
 }
 
@@ -23,21 +30,24 @@ int print_chara(va_list list, char *buffer, int *buffer_index)
  */
 int print_string(va_list list, char *buffer, int *buffer_index)
 {
-	char *str = va_arg(list, char *);
-	int length = j;
+	char *str;
+	int i;
 
+	str = va_arg(list, char *);
 	if (str == NULL)
 		str = "(null)";
 
-	for (j = 0; str[j]; j++)
-	{
-		buffer[(*buffer_index)++] = str[j];
-		if (*buffer_index == BUFFER_SIZE)
+	for (i = 0; str[i]; i++)
 		{
-			write(1, buffer, BUFFER_SIZE);
-			*buffer_index = 0;
+			buffer[(*buffer_index)++] = str[i];
+			if (*buffer_index == BUFFER_SIZE)
+				{
+				write(1, buffer, BUFFER_SIZE);
+				*buffer_index = 0;
+				}
 		}
-	return (1);
+
+		return (i);
 }
 
 /**
@@ -46,9 +56,15 @@ int print_string(va_list list, char *buffer, int *buffer_index)
  * @buffer_index: The place the character is stored
  * @Return: the number of character printed
  */
-int print_percent(va_list list, char *buffer, int *buffer_index)
+int print_percent(va_list list  __attribute__((unused)), char *buffer, int *buffer_index)
 {
-	buffer[*buffer_index] = '%';
-	(*buffer_index)++;
+
+	buffer[(*buffer_index)++] = '%';
+
+	if (*buffer_index == BUFFER_SIZE)
+	{
+	write(1, buffer, BUFFER_SIZE);
+	*buffer_index = 0;
+	}
 	return (1);
 }
