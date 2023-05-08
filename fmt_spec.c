45ad399bdc010db3d0528842b723351c460dd503
konyhea <ndukaprincek@gmail.com>
2023-05-07 19:17:49 -0700

    update version

--- fmt_spec.c
+++ fmt_spec.c
@@ -2,9 +2,10 @@
 
 /**
  * print_chara - Function that prints conversion specifier chara
+ * @list: list of arguments
  * @buffer: stores character at the index
  * @buffer_index: The place the character is stored
- * @Return: number of character printed
+ * Return: number of character printed
  */
 
 int print_chara(va_list list, char *buffer, int *buffer_index)
@@ -13,20 +14,20 @@ int print_chara(va_list list, char *buffer, int *buffer_index)
 
 	c = va_arg(list, int);
 	buffer[*buffer_index] = c;
-	
 	if (*buffer_index == BUFFER_SIZE)
 	{
 		write(1, buffer, BUFFER_SIZE);
 		*buffer_index = 0;
-	}	
+	}
 	return (1);
 }
 
 /**
  * print_string - Function that prints string
+ * @list: list of srguments
  * @buffer: stores string at the index
  * @buffer_index: The place the character is stored
- * @Return: the number of character printed
+ * Return: the number of character printed
  */
 int print_string(va_list list, char *buffer, int *buffer_index)
 {
@@ -35,28 +36,30 @@ int print_string(va_list list, char *buffer, int *buffer_index)
 
 	str = va_arg(list, char *);
 	if (str == NULL)
-		str = "(null)";
+	str = "(null)";
 
 	for (i = 0; str[i]; i++)
-		{
-			buffer[(*buffer_index)++] = str[i];
-			if (*buffer_index == BUFFER_SIZE)
-				{
-				write(1, buffer, BUFFER_SIZE);
-				*buffer_index = 0;
-				}
-		}
+	{
+		buffer[(*buffer_index)++] = str[i];
+		if (*buffer_index == BUFFER_SIZE)
+			{
+			write(1, buffer, BUFFER_SIZE);
+			*buffer_index = 0;
+			}
+	}
 
 		return (i);
 }
 
 /**
- * print_string - Function that prints string
+ * print_percent - Function that prints percent
+ * @list: list of arguments
  * @buffer: stores string at the index
  * @buffer_index: The place the character is stored
- * @Return: the number of character printed
+ * Return: the number of character printed
  */
-int print_percent(va_list list  __attribute__((unused)), char *buffer, int *buffer_index)
+int print_percent(va_list list  __attribute__((unused)), char *buffer,
+int *buffer_index)
 {
 
 	buffer[(*buffer_index)++] = '%';
